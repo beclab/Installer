662a9f646fc8454061f0765df7aeeb4ec11893f6
aby913 <aby913@163.com>
2024-07-29 22:50:55 +0800

    fix: get ks version

--- pkg/pipelines/initialize_terminus.go
+++ pkg/pipelines/initialize_terminus.go
@@ -114,7 +114,16 @@ func getNodeVersion(kubeType string, minikube bool) (string, error) {
 	var cmd = fmt.Sprintf("/usr/local/bin/kubectl get nodes -o jsonpath='{.items[0].status.nodeInfo.kubeletVersion}'")
 	stdout, _, err := util.Exec(cmd, false, false)
 	if err != nil {
-		return ver, err
+		switch kubeType { // todo minikube 应该要查一个 node 的信息才对
+		case common.K8s:
+			ver = common.DefaultK8sVersion
+		case common.K3s:
+			fallthrough
+		default:
+			ver = common.DefaultK3sVersion
+		}
+		return ver, nil
+		// return ver, err
 	}
 
 	if strings.Contains(stdout, "k3s") {
