0b227be88469759655563b6f1e6d3ec0dc949501
aby913 <aby913@163.com>
2024-08-25 22:05:28 +0800

    fix: remove comment

--- pkg/phase/system/prepare.go
+++ pkg/phase/system/prepare.go
@@ -4,44 +4,49 @@ import (
 	"strings"
 
 	"bytetrade.io/web3os/installer/pkg/binaries"
+	"bytetrade.io/web3os/installer/pkg/bootstrap/os"
+	"bytetrade.io/web3os/installer/pkg/bootstrap/patch"
 	"bytetrade.io/web3os/installer/pkg/bootstrap/precheck"
 	"bytetrade.io/web3os/installer/pkg/common"
 	"bytetrade.io/web3os/installer/pkg/constants"
+	"bytetrade.io/web3os/installer/pkg/container"
 	"bytetrade.io/web3os/installer/pkg/core/module"
 	"bytetrade.io/web3os/installer/pkg/core/pipeline"
+	"bytetrade.io/web3os/installer/pkg/gpu"
 	_ "bytetrade.io/web3os/installer/pkg/gpu"
+	"bytetrade.io/web3os/installer/pkg/images"
 	"bytetrade.io/web3os/installer/pkg/kubesphere/plugins"
 	"bytetrade.io/web3os/installer/pkg/storage"
 	"bytetrade.io/web3os/installer/pkg/terminus"
 )
 
 func PrepareSystemPhase(runtime *common.KubeRuntime) *pipeline.Pipeline {
-	// var isK3s = strings.Contains(runtime.Arg.KubernetesVersion, "k3s")
-	// var osSupport = isSupportOs()
+	var isK3s = strings.Contains(runtime.Arg.KubernetesVersion, "k3s")
+	var osSupport = isSupportOs()
 	m := []module.Module{
 		&precheck.GetSysInfoModel{},
 		&plugins.CopyEmbed{},
 		&terminus.TidyPackageModule{},
 		&storage.DownloadStorageBinariesModule{},
 		//
-		// &precheck.PreCheckOsModule{},
-		// &patch.InstallDepsModule{},
-		// &os.ConfigSystemModule{},
+		&precheck.PreCheckOsModule{},
+		&patch.InstallDepsModule{},
+		&os.ConfigSystemModule{},
 		// &binaries.K3sNodeBinariesModule{},
 		&binaries.NodeBinariesModule{},
-		// &container.InstallContainerModule{Skip: isK3s, NoneCluster: true}, //
+		&container.InstallContainerModule{Skip: isK3s, NoneCluster: true}, //
 		// &k3s.InstallContainerModule{Skip: !isK3s},
-		// &images.PreloadImagesModule{Skip: runtime.Arg.SkipPullImages}, //
+		&images.PreloadImagesModule{Skip: runtime.Arg.SkipPullImages}, //
 		&terminus.CopyToWizardModule{},
 	}
 
-	// m = append(m,
-	// 	&gpu.InstallDepsModule{Skip: !runtime.Arg.GPU.Enable || !osSupport},
-	// 	&gpu.RestartK3sServiceModule{Skip: !runtime.Arg.GPU.Enable || !osSupport},
-	// 	&gpu.RestartContainerdModule{Skip: !runtime.Arg.GPU.Enable || !osSupport},
-	// 	&gpu.InstallPluginModule{Skip: true},
-	// 	&terminus.PreparedModule{},
-	// )
+	m = append(m,
+		&gpu.InstallDepsModule{Skip: !runtime.Arg.GPU.Enable || !osSupport},
+		&gpu.RestartK3sServiceModule{Skip: !runtime.Arg.GPU.Enable || !osSupport},
+		&gpu.RestartContainerdModule{Skip: !runtime.Arg.GPU.Enable || !osSupport},
+		&gpu.InstallPluginModule{Skip: true},
+		&terminus.PreparedModule{},
+	)
 
 	return &pipeline.Pipeline{
 		Name:    "Prepare the System Environment",
