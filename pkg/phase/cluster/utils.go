0ec0779e10a93eba368988b7e84da37c19044e3c
aby913 <aby913@163.com>
2024-11-07 14:16:31 +0800

    fix: Install wsl without a gpu, exclude the installation of nvidia device plugins

--- pkg/phase/cluster/utils.go
+++ pkg/phase/cluster/utils.go
@@ -2,10 +2,20 @@ package cluster
 
 import (
 	"bytetrade.io/web3os/installer/pkg/core/module"
+	"bytetrade.io/web3os/installer/pkg/gpu"
 )
 
 type phase []module.Module
 
 func (p phase) addModule(m ...module.Module) phase {
 	return append(p, m...)
+}
+
+type gpuModuleBuilder func() []module.Module
+
+func (m gpuModuleBuilder) withGPU() []module.Module {
+	if (&gpu.CheckWslGPU{}).CheckNvidiaSmiFileExists() {
+		return m()
+	}
+	return nil
 }
