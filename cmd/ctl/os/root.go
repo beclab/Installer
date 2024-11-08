e8adb2f15855b1c624e0a457a28143249e5717d6
dkeven <dkvvven@gmail.com>
2024-11-08 20:13:03 +0800

    feat: change root os command to olares

--- cmd/ctl/os/root.go
+++ cmd/ctl/os/root.go
@@ -8,15 +8,12 @@ import (
 
 func NewCmdOs() *cobra.Command {
 	rootOsCmd := &cobra.Command{
-		Use:   "terminus",
+		Use:   "olares",
 		Short: "Operations such as installing and uninstalling Olares can be performed using the --phase parameter, which allows for actions like downloading installation packages, downloading dependencies, and installing Olares.",
 	}
 
 	_ = exec.Command("/bin/bash", "-c", "ulimit -u 65535").Run()
 	_ = exec.Command("/bin/bash", "-c", "ulimit -n 65535").Run()
-
-	// TODO Add a command to install Terminus.
-	// TODO Before installing Terminus, we need to obtain user information, the WSL NAT gateway address, etc.
 
 	rootOsCmd.AddCommand(NewCmdRootDownload())
 	rootOsCmd.AddCommand(NewCmdPrepare())
