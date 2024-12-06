3385cf57f3a7a521361f4c5c9950a8502a7e96c8
dkeven <dkvvven@gmail.com>
2024-12-06 21:10:33 +0800

    feat: upgrade cuda driver to 550

--- pkg/gpu/tasks.go
+++ pkg/gpu/tasks.go
@@ -87,12 +87,12 @@ func (t *InstallCudaDriver) Execute(runtime connector.Runtime) error {
 		return errors.Wrap(errors.WithStack(err), "Failed to apt-get update")
 	}
 
-	if _, err := runtime.GetRunner().Host.SudoCmd("apt-get -y install nvidia-kernel-open-545", false, true); err != nil {
-		return errors.Wrap(errors.WithStack(err), "Failed to apt-get install nvidia-kernel-open-545")
+	if _, err := runtime.GetRunner().Host.SudoCmd("apt-get -y install nvidia-kernel-open-550", false, true); err != nil {
+		return errors.Wrap(errors.WithStack(err), "Failed to apt-get install nvidia-kernel-open-550")
 	}
 
-	if _, err := runtime.GetRunner().Host.SudoCmd("apt-get -y install nvidia-driver-545", false, true); err != nil {
-		return errors.Wrap(errors.WithStack(err), "Failed to apt-get install nvidia-driver-545")
+	if _, err := runtime.GetRunner().Host.SudoCmd("apt-get -y install nvidia-driver-550", false, true); err != nil {
+		return errors.Wrap(errors.WithStack(err), "Failed to apt-get install nvidia-driver-550")
 	}
 
 	// if _, err := runtime.GetRunner().Host.SudoCmd("apt-get -y install cuda-12-1", false, true); err != nil {
