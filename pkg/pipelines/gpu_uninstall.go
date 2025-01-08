af1588169bb3b0e4d001ac2b6ad4be732780a3b6
dkeven <dkvvven@gmail.com>
2025-01-08 18:46:40 +0800

    fix: refuse to uninstall gpu from inside WSL

--- pkg/pipelines/gpu_uninstall.go
+++ pkg/pipelines/gpu_uninstall.go
@@ -5,11 +5,17 @@ import (
 	"bytetrade.io/web3os/installer/pkg/core/module"
 	"bytetrade.io/web3os/installer/pkg/core/pipeline"
 	"bytetrade.io/web3os/installer/pkg/gpu"
+	"fmt"
+	"os"
 )
 
 func UninstallGpuDrivers() error {
 
 	arg := common.NewArgument()
+	if arg.SystemInfo.IsWsl() {
+		fmt.Println("WSL's GPU driver is managed by Windows, does not support uninstalling from inside.")
+		os.Exit(1)
+	}
 	arg.SetConsoleLog("gpuuninstall.log", true)
 
 	runtime, err := common.NewKubeRuntime(common.AllInOne, *arg)
