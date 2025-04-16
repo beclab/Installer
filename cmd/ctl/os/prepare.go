06cd6e153413c2ae229a0bc528c0d435ac2bcfa5
dkeven <dkvvven@gmail.com>
2025-04-16 18:55:44 +0800

    feat: support seperate component args in prepare

--- cmd/ctl/os/prepare.go
+++ cmd/ctl/os/prepare.go
@@ -1,30 +1,33 @@
 package os
 
 import (
+	"log"
+
 	"bytetrade.io/web3os/installer/cmd/ctl/options"
 	"bytetrade.io/web3os/installer/pkg/pipelines"
 	"github.com/spf13/cobra"
-	"log"
 )
 
 type PrepareSystemOptions struct {
 	PrepareOptions *options.CliPrepareSystemOptions
+	Components     []string
 }
 
 func NewPrepareSystemOptions() *PrepareSystemOptions {
 	return &PrepareSystemOptions{
 		PrepareOptions: options.NewCliPrepareSystemOptions(),
+		Components:     []string{},
 	}
 }
 
 func NewCmdPrepare() *cobra.Command {
 	o := NewPrepareSystemOptions()
 	cmd := &cobra.Command{
-		Use:   "prepare",
+		Use:   "prepare [component1 component2 ...]",
 		Short: "Prepare install",
 		Run: func(cmd *cobra.Command, args []string) {
-
-			if err := pipelines.PrepareSystemPipeline(o.PrepareOptions); err != nil {
+			o.Components = args
+			if err := pipelines.PrepareSystemPipeline(o.PrepareOptions, o.Components); err != nil {
 				log.Fatalf("error: %v", err)
 			}
 		},
